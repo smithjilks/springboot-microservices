be4af014caae68a1797a0169e713838986cc1399
Jilks Smith <jilks.smith@cellulant.io>
2023-12-07 21:15:34 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,4 @@
-include("product-service", "review-service", "recommendation-service", "product-composite-service")
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+include("product-service", "review-service", "recommendation-service", "product-composite-service", "api", "util")
