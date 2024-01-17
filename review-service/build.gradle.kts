a20cc0774046359c3f8dc1608decdf60fcda8716
Jilks Smith <jilks.smith@cellulant.io>
2024-01-17 20:59:27 +0300

    Remove unnecessary mongodb dependency in review service build.gradle.kts

--- review-service/build.gradle.kts
+++ review-service/build.gradle.kts
@@ -36,7 +36,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("org.postgresql:postgresql")
 
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("org.springframework.cloud:spring-cloud-starter-stream-rabbit")
     implementation("org.springframework.cloud:spring-cloud-starter-stream-kafka")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
